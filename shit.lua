9177ba57f6e450dcae92f169d93af13c8a5af2e7
Haxardous <30158626+Haxardous@users.noreply.github.com>
2026-02-13 22:36:11 +0300

    fixed the crap

--- shit.lua
+++ shit.lua
@@ -3,51 +3,34 @@
 -- Haxardous
 -----------------------------------------------------
 
-function startAhShit()
-    local timehour, timeminute = getTime()
+function drawText()
+    dxDrawText("Ah shit, here we go again.", 859 + 1, 919 + 1, 1061 + 1, 938 + 1, tocolor(1, 0, 0, 255), 1.30, "default-bold", "left", "top", false, false, false, false, false)
+    dxDrawText("Ah shit, here we go again.", 859, 919, 1061, 938, tocolor(255, 255, 255, 255), 1.30, "default-bold", "left", "top", false, false, false, false, false)
+end
 
+function startAhShit()
     -- set CJ's skin model.
-    if not (getElementModel(localPlayer) == 0) then
-        setElementModel(localPlayer, 0)
-        print("skin model set to 0 (CJ Skin)")
-    end
+    setElementModel(localPlayer, 0)
+    print("skin model set to 0 (CJ Skin)")
 
     -- set CJ's clothes.
     addPedClothes(localPlayer, "vest", "vest", 0)
     print("CJ's vest set to 0 (Clothes)")
 
-    -- set weather
-    if not (getWeather() == 0) then
+    -- set weather & time
         setWeather(0)
-        print("weather set to 0")
-    elseif not (timerhour == 12) then
         setTime(12, 0)
-        print("time set to 12:0")
-    end
+
     -- playing sound
-    sound = playSound("ahshitherewegoagain.mp3")
+    local sound = playSound("ahshitherewegoagain.mp3")
     setSoundVolume(sound, 100)
     print("playing sound")
 
     -- hiding HUD & chat
-    local components = { "ammo", "area_name" ,"armour", "breath", "clock", "health", "money",
-        "radar", "vehicle_name", "weapon", "radio", "wanted", "crosshair" }
-        
-        for _,component in ipairs(components) do
-            if isPlayerHudComponentVisible(component) then
-                setPlayerHudComponentVisible("all", false)
-                print("hud has been hidden successfully")
-                if (isChatVisible() == true) then
-                    showChat(false)
-                    print("chat has been hidden successfully")
-                end
-            end
-        end
+    setPlayerHudComponentVisible("all", false)
+    showChat(false)
 
-    function drawText()
-        dxDrawText("Ah shit, here we go again.", 859 + 1, 919 + 1, 1061 + 1, 938 + 1, tocolor(1, 0, 0, 255), 1.30, "default-bold", "left", "top", false, false, false, false, false)
-        dxDrawText("Ah shit, here we go again.", 859, 919, 1061, 938, tocolor(255, 255, 255, 255), 1.30, "default-bold", "left", "top", false, false, false, false, false)
-    end
+    -- render text
     addEventHandler("onClientRender", root, drawText)
 
     -- position & camera
